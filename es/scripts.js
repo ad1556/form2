ee5d122dae2360bb61f5aec761248620532e2664
ad1556 <101182705+ad1556@users.noreply.github.com>
2023-11-21 20:00:19 +0200

    Add files via upload

---
+++ es/scripts.js
@@ -0,0 +1,18 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+    console.log('Login:', { username, password });
+});
+
+document.getElementById('registerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('registerUsername').value;
+    const password = document.getElementById('registerPassword').value;
+    const confirmPassword = document.getElementById('registerConfirmPassword').value;
+    if (password !== confirmPassword) {
+        alert('Passwords do not match.');
+        return;
+    }
+    console.log('Registration:', { username, password });
+});
